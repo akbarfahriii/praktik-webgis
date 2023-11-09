32679019e46413cdb430e459d14f2683545e2b0d
akbarfahriii <150332515+akbarfahriii@users.noreply.github.com>
2023-11-09 15:39:34 +0700

    import qgis2webfiles

---
+++ layers/ServiceAreaND_13_DirtyAreas_0.js
@@ -0,0 +1,1 @@
+var json_ServiceAreaND_13_DirtyAreas_0 = {"type":"FeatureCollection","name":"ServiceAreaND_13_DirtyAreas_0","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"ObjectID":"1","IsRetired":0.0,"DirtyArea_Length":0.0,"DirtyArea_Area":0.0},"geometry":{"type":"MultiPolygon","coordinates":[[]]}}]}
